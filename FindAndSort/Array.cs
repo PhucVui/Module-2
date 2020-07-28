289173ce1db68752e39c11140b644e59dd3a07cf
Phuc Vui <phucvui16495@gmail.com>
2020-07-28 23:44:30 +0700

    new

---
+++ FindAndSort/Array.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindAndSort
+{
+    class Array
+    {
+        public int[] CreateArray(int size)
+        {
+            Random random = new Random();
+            int[] Array = new int[size];
+            for (int i = 0; i < Array.Length; i++)
+            {
+                Array[i] = random.Next(30, 60);
+            }
+            return Array;
+        }
+
+        public void PrintArray(int [] Array)
+        {
+            string array = "";
+
+            for (int i = 0; i < Array.Length; i++)
+            {
+                array+= $"{Array[i]}\t";                
+            }
+            Console.WriteLine(array);
+            }
+            
+
+        public bool IsSymmetryArray(int [] newArray)
+        {
+            for(int i =0; i < newArray.Length; i++)
+            {
+                if(newArray[i] != newArray[newArray.Length -i -1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool CheckSort(int[] arr)
+        {
+            for (int i=0;i<arr.Length-1;i++)
+            {
+                if (arr[i]>=arr[i+1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public void SelectionSort(int [] Array1)
+        {
+            for(int i =0; i < Array1.Length-1; i++)
+            {
+                int min = Array1[i];
+                int pos = i;
+                for(int j=i+1; j< Array1.Length; j++)
+                {
+                   if(Array1[j]<min)
+                    {
+                        min = Array1[j];
+                        pos = j;
+                    }                    
+                }
+                int temp;
+                temp = Array1[i];
+                Array1[i] = Array1[pos];
+                Array1[pos] = temp;
+            }
+        }
+
+        public int Find(int[] Array, int key) 
+        {
+            int First = 0;
+            int Last = Array.Length - 1;
+            while (Last >= First) 
+            {
+                int mid = (First + Last) / 2;
+
+                if (key < Array[mid])
+                {
+                    Last = mid - 1;  
+                }
+                else if(key == Array[mid])
+                {
+                    return mid;
+                }
+                else
+                {
+                    First = mid + 1; 
+                }
+            } return -1;           
+        }
+     }
+}
---
+++ FindAndSort/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Immutable;
+
+namespace FindAndSort
+{
+    class Program
+    {
+        public static Array ArrCls = new Array();
+        public static int[] array;
+        public static void Menu()
+        {
+            Console.WriteLine("1.Create Array");
+            Console.WriteLine("2.Check SymmetryArray");
+            Console.WriteLine("3.Sort Array");
+            Console.WriteLine("4.Find Array");
+            Console.WriteLine("5.Exit");          
+        }
+
+        public static void Create()
+        {
+            array = new int[0];
+            Console.WriteLine($"Enter size:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            array = ArrCls.CreateArray(n);
+            ArrCls.PrintArray(array);
+        }
+
+        static void CheckSymmetryArray()
+        {          
+            Console.WriteLine($"Array Is {(!ArrCls.IsSymmetryArray(array) ? "Not" : "")} Symmetry");
+        }
+
+        static void SortArray()
+        {   
+            ArrCls.SelectionSort(array);
+            ArrCls.PrintArray(array);
+        }
+
+         public static void FindArray()
+        {
+            if (ArrCls.CheckSort(array))
+            {
+                Console.WriteLine($"Enter number to find:");
+                int num = int.Parse(Console.ReadLine());
+                int index = ArrCls.Find(array, num);
+                if (index == -1)
+                {
+                    Console.WriteLine("not found");
+                }
+                else
+                {
+                    Console.WriteLine($"found at: {index}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("not yet sort");
+            }           
+        }
+
+        public static  void Main()
+        {
+            do
+            {
+                Menu();
+                int choice = int.Parse(Console.ReadLine());
+                if(choice == 5)
+                {
+                    break;
+                }
+                switch(choice)
+                {
+                    case 1: Create();                       
+                        break;
+                    
+                    case 2: CheckSymmetryArray();                        
+                        break;
+                    
+                    case 3: SortArray();                      
+                        break;
+                    
+                    case 4: FindArray();
+                        break;
+                }               
+            } while (true);
+        }
+
+
+    }    
+}
